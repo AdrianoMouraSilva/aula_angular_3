ff686ccbdfdcae92cb3b69f1f2058da8f840ea95
ADRIANO MOURA DA SILVA <adriano.msilva28@senacsp.edu.br>
2025-08-01 22:17:00 -0300

    first commit_2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OlaMundo } from './components/ola-mundo/ola-mundo';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,OlaMundo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('aula03_1');
+  // protected readonly title = signal('aula03_1');
 }

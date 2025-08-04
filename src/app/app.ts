a9cd110ed8ee078aadf929ff6e901c1b780aabfc
ADRIANO MOURA DA SILVA <adriano.msilva28@senacsp.edu.br>
2025-08-04 19:30:57 -0300

    Perfil

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OlaMundo } from './components/ola-mundo/ola-mundo';
+import { Perfil } from './components/perfil/perfil';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,OlaMundo],
+  imports: [RouterOutlet,OlaMundo,Perfil],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
